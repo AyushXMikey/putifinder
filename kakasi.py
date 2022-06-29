9b36052d5d5b2c450134efc2b0c8daadf6da9c7d
AyushXMikey <95029583+AyushXMikey@users.noreply.github.com>
2022-06-29 19:28:24 +0545

    Add files via upload

---
+++ kakasi.py
@@ -0,0 +1,34 @@
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+print(".")
+
+
+
+
+
+
+print(".....KAKASSSII X AAYUSSHHH.......")
+print("......PUTTII WALI MAIYAA.........")
+
+
+a = input("Enter Size of your lado:  ")
+b = input("Enter Size of the condom you are using:  ")
+a = int(a)
+b = int(b)
+puti = (a*b)/2
+print("your puti size is:  ", puti)
+
+
+
+# ULULULU
